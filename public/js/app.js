e9ece3c0bf09ad25e696bfe3d448becfef95ef68
mean <gnenortas@gmail.com>
2021-04-05 13:13:07 -0400

    Removed unnecessary consol.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
@@ -19,6 +11,7 @@ weatherForm.addEventListener('submit',(e) => {
 
     const location = search.value
     messageOne.textContent = 'Loading ...'
+    messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address='+location).then((response) => {
         response.json().then((data) => {
